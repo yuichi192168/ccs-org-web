76fec9a68f0f9c0d042c03170480f621845c6fce
yuichi192168 <yuichi192168@gmail.com>
2026-04-30 21:12:02 +0800

    build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    tsconfigPath: './tsconfig.json',
   },
 }
 
